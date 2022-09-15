ba41552892db40dc58ef99d0d6ba4b1e6ba2413c
Gabriel Oliveira <oliveiragabriel1712@gmail.com>
2022-09-15 10:43:58 -0300

    configurando cores

--- web/tailwind.config.cjs
+++ web/tailwind.config.cjs
@@ -7,7 +7,9 @@ module.exports = {
   theme: {
     extend: {
       backgroundImage: {
-        galaxy: "url('/public/background-galaxy.png')"
+        galaxy: "url('/public/background-galaxy.png')",
+        'nwl-gradient': 'linear-gradient(90deg, #9572FC 0%, #43E7AD 50.00%, #E1D55D 100%)',
+        'game-gradient': 'linear-gradient(180deg, rgba(0, 0, 0, 0) 0%, rgba(0, 0, 0, 0.9) 67.08%)'
       }
     },
   },
