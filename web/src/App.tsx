7fcac0e6195ac63658beec7ca0b48090c23db701
Gabriel Oliveira <oliveiragabriel1712@gmail.com>
2022-09-16 11:01:49 -0300

    Finalizando front-end

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,3 +1,4 @@
+import { MagnifyingGlassPlus } from "phosphor-react";
 import "./styles/main.css";
 import logoImg from "./assets/logo-nlw-esports.svg";
 
@@ -71,7 +72,7 @@ function App() {
         </a>
       </div>
 
-      <div className="pt-1 bg-nwl-gradient self-stretch rounded-lg overflow-hidden mt-8">
+      <div className="pt-1 bg-nwl-gradient self-stretch rounded-lg mt-8 overflow-hidden ">
         <div className="bg-[#2A2634] px-8 py-6 flex justify-between items-center">
           <div>
             <strong className="text-2xl text-white font-black block">
@@ -82,7 +83,8 @@ function App() {
             </span>
           </div>
 
-          <button className="py-3 px-4 bg-violet-500 hover:bg-violet-600 text-white rounded">
+          <button className="py-3 px-4 bg-violet-500 hover:bg-violet-600 text-white rounded flex items-center gap-3">
+            <MagnifyingGlassPlus size={24} />
             Publicar anuncio
           </button>
         </div>
